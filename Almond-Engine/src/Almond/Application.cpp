8dd4d5e5c31fb4f5809223e3f047c25761da3fe0
Nick <theerapat.nick@gmail.com>
2022-01-15 12:10:17 +0700

    entry point tutorial

---
+++ Almond-Engine/src/Almond/Application.cpp
@@ -0,0 +1,24 @@
+#include "Application.h"
+
+namespace Almond {
+
+	Application::Application()
+	{
+
+
+	}
+
+	virtual Application::~Application()
+	{
+
+	}
+
+	Application::run()
+	{
+		while (true)
+		{
+
+		}
+	}
+
+}
---
+++ Almond-Engine/src/Almond/Application.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace Almond {
+
+	class Application
+	{
+
+	public:
+
+		Application();
+		~Application();
+
+	};
+
+}
